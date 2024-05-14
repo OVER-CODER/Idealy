53ebb4c4e62f8ce9b5560f44cde9a18a94cddda6
Aryan Pandit <aryanpandit.1205@gmail.com>
2024-05-15 02:40:49 +0530

    minor strictQuery changes

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 let isConnected = false;
 
 export const connectToDB = async () => {
-//  mongoose.set('strictQuery', true);
+ mongoose.set('strictQuery', true);
  if(!process.env.MONGODB_URI) {
    return console.log('MongoDB URL is missing');
  }
