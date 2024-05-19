80cb9c408fccf136688a1f35d39e7a9b8d6f9cde
Aryan Pandit <aryanpandit.1205@gmail.com>
2024-05-19 22:44:59 +0530

    user action bug fixes

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,10 +1,12 @@
 "use server"
 
 
+import { FilterQuery, SortOrder } from "mongoose";
 import Idea from "../models/idea.model";
 import User from "../models/user.model"
 import { connectToDB } from "../mongoose"
 import { revalidatePath } from "next/cache";
+import { skip } from "node:test";
 
 
 interface Params {
@@ -88,5 +90,54 @@ export async function fetchUserPosts(userId: string) {
       return ideas;
   } catch (error:any) {
     throw new Error(`Error fetching user posts: ${error.message}`);
+  }
+}
+
+
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+} : {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+    const regex = new RegExp(searchString, 'i'); 
+
+    const query: FilterQuery <typeof User>= {
+      id: { $ne: userId },
+    }
+
+    if(searchString.trim() !== ''){
+      query.$or = [
+        { username: {$regex: regex} },
+        { name: {$regex: regex} }
+      ]
+    }
+
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await usersQuery.exec();
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext};
+  } catch (error: any) {
+    throw new Error(`Error fetching users: ${error.message}`);
   }
 }
