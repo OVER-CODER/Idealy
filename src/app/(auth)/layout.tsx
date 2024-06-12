3065771c4ecf4017c276d57d0e822a1a83bcbf1c
Aryan Pandit <aryanpandit.1205@gmail.com>
2024-06-12 20:22:06 +0530

    code refubrishing

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,26 +1,3 @@
-// import { ClerkProvider } from "@clerk/nextjs";
-// import '../globals.css';
-// import { Inter } from "next/font/google";
-
-// export const metadata = {
-//     title: "Idealy",
-//     description: "",
-// };
-
-// const inter = Inter({subsets: ["latin"]});
-
-// export default function RootLayout({children}: {children: React.ReactNode}) {
-//     return(
-//         <ClerkProvider>
-//             <html lang="en">
-//                 <body className={`${inter.className} bg-dark-1`}>
-//                     {children}
-//                 </body>
-//             </html>
-//         </ClerkProvider>
-//     )
-// }
-
 
 import React from "react";
 import type { Metadata } from "next";
